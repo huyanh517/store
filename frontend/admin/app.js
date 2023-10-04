690b5f97066819a478123ace8b0fde826e4bdca0
huyanh <58281984+huyanh517@users.noreply.github.com>
2023-10-04 19:17:13 +0700

    setup

--- frontend/admin/app.js
+++ frontend/admin/app.js
@@ -1,2 +1,21 @@
+$.ajax({
+    type: "GET",
+    url: "http://localhost:3000/products",
+    success: function (response) {
+        displayData(response);
+    }
+});
 
 
+function displayData(data) {
+    $(data).each((index, item) => {
+        let tr = "<tr>";
+        tr    += "<td scope='row'>" + Number(index + 1) + "</td>";
+        tr    += "<td>" + item.title + "</td>";
+        tr    += "<td>" + item.category + "</td>";
+        tr    += "<td>" + "$" + item.price + "</td>";
+        tr    += "</tr";
+
+        $(".tbody-products").append(tr);
+    })
+}
